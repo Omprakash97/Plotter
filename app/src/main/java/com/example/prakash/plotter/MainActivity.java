b396825696c957cff2a13507c18d5c8f8f893bf4
Omprakash97 <r.omprakash1997@gmail.com>
2019-06-11 11:09:05 +0530

    WITH COMMENTS

--- app/src/main/java/com/example/prakash/plotter/MainActivity.java
+++ app/src/main/java/com/example/prakash/plotter/MainActivity.java
@@ -17,32 +17,46 @@ public class MainActivity extends AppCompatActivity {
     SQLiteDatabase db;
     GraphView graphView;
     LineGraphSeries series;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         coor = (EditText)findViewById(R.id.cood);
         graphView = findViewById(R.id.graphView);
 
         series= new LineGraphSeries();
         graphView.addSeries(series);
 
+        // CREATE A DATABASE
         db = openOrCreateDatabase("plots",MODE_PRIVATE,null);
 
-      //  plots();
+        // CREATING A TABLE
+        db.execSQL("CREATE TABLE IF NOT EXISTS xy(id INTEGER PRIMARY KEY AUTOINCREMENT, x INTEGER, y INTEGER);");
+
+        // TRY PLOTTING ON CREATE OF THE APP :
+        plots();
     }
 
+    // PLOT NEW POINTS BY CLICKING ON UPDATE
     public void update(View v){
         plots();
     }
 
     public void plots(){
+        // TRY TO READ ALL THE POINTS
         Cursor r = db.rawQuery("Select * from xy",null);
+
+        // CHECK THE NO. OF LINES (POINTS TO PLOT)
         Integer count = r.getCount();
         Integer i=0;
 
+        // PLOT THE GRAPH ONLY IF THERE ARE SOME DATA IN THE TABLE
         if(count > 0){
-          DataPoint[] dp = new DataPoint[count];
+
+            DataPoint[] dp = new DataPoint[count];
+
             if (r.moveToFirst()) {
                 do {
                     dp[i] = new DataPoint(r.getInt(0),r.getInt(1));
@@ -52,18 +66,20 @@ public class MainActivity extends AppCompatActivity {
             r.close();
 
         series.resetData(dp);
+
         }
     }
 
     public void push(View v){
+
+        // INSTED OF GETTING FROM THIS BOX U CAN GET IT FROM OUR "MSG"
         String data = coor.getText().toString();
         String[] d = data.split(",");
         Integer x,y;
+
+        // ASSIGN 2 VALUES HERE
         x=Integer.parseInt(d[0]);
         y=Integer.parseInt(d[1]);
-
-        // CREATING A TABLE
-        db.execSQL("CREATE TABLE IF NOT EXISTS xy(id INTEGER PRIMARY KEY AUTOINCREMENT, x INTEGER, y INTEGER);");
 
         // CHECKING IF THE ENTRY IS GREATER THAT 50 , AND CLEAR THE TABLE IF IT EXCEEDS
 
